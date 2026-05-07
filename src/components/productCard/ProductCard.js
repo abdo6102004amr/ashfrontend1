fe6edc4c1cb2d3ba0e3d7d4bc1e5d6fbd6aabb84
abdo6102004amr <abdoamr6102004@gmail.com>
2026-05-07 20:03:35 +0300

    Update ProductCard.js

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -6,9 +6,13 @@ import './ProductCard.css'
 const ProductCard = ({ product }) => {
   const { id, image, name, price, available, sizePrices = {}, sizeSoldOut = {} } = product
   const { addToCart } = useCart();
-  const sizeOptions = ['70 ml', '250 ml'];
-  const [selectedSize, setSelectedSize] = useState('250 ml');
+  const sizeOptions = product.sizes || ['10 ml', '100 ml', '250 ml'];
 
+  const [selectedSize, setSelectedSize] = useState(
+    sizeOptions.includes('100 ml')
+      ? '100 ml'
+      : sizeOptions[0]
+  );
   // 🔥 ADD (حل مشكلة اختلاف 70ml و 70 ml)
   const normalize = (s) => s.replace(/\s/g, '').toLowerCase();
 
